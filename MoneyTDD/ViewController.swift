486125902f8529e27c9a9bfb8ac3af7f1b402b4a
easthelper <easthelper@gmail.com>
2016-01-17 01:42:42 +0900

    Initial Commit

---
+++ MoneyTDD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoneyTDD
+//
+//  Created by 손동우 on 2016. 1. 17..
+//  Copyright © 2016년 easthelper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
